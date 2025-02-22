3e44771246d8ee9a323d0ea6225fe22ee34a3482
Kuntiarso <gilang.grk17@gmail.com>
2025-02-22 17:31:37 +0700

    feat(customer-query-service): prepare query service for elastic

---
+++ customerqueryservice/src/test/java/com/developer/superuser/customerqueryservice/CustomerqueryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.superuser.customerqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerqueryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
